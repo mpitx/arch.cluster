e6f93ba0d061558b16e3e65c9c21e31b0a4545f8
kennyballou <kballou@onyx.boisestate.edu>
2013-02-27 16:56:15 -0700

    Add more setup methods

--- node-install.py
+++ node-install.py
@@ -4,6 +4,7 @@ import os
 import configparser
 from subprocess import call
 import sys
+import shutil
 import logging
 
 logger = logging.GetLogger("install")
@@ -80,3 +81,114 @@ def umount_partitions():
         umount_cmd = ["umount", m]
         logger.debug(umount_cmd)
         #call(umount_cmd)
+
+def bootstrap_system():
+    pacstrap_cmd = ["pacstrap", "/mnt", "base", "base-devel"]
+    logger.debug(pacstrap_cmd)
+    #call(pacstrap_cmd)
+    logger.debug("Copying basic config files over to new root")
+    #shutil.copytree("/mntfiles", "/mnt/")
+    genfstab_cmd = ["genfstab", "-U", "-p", "/mnt"]
+    logger.debug("%s >> /mnt/etc/fstab" % genfstab_cmd)
+    #with open("/mnt/etc/fstab", "a") as fstab:
+        #call(genfstab, stdout=fstab)
+
+def live_install_main():
+    prepare_disks()
+    mount_partitions()
+    bootstrap_system()
+    logger.debug("Copy and call self from chroot")
+    shutil.copy2(os.join.path(os.getcwd(), "node-install.py"),
+                 "/mnt/root/node-install.py")
+    chroot_cmd = ["arch-chroot",
+                  "/mnt",
+                  "python",
+                  "node-install.py",
+                  "node-number",
+                  "in-chroot"]
+    logger.debug(chroot_cmd)
+
+    os.remove("/mnt/root/node-install.py")
+
+    umount_partitions()
+
+def chroot_stage_main():
+    hostname = getConfig("HOST", "NAME")
+    zoneInfo = getConfig("HOST", "ZONEINFO")
+    domain = getConfig("NETWORK", "DOMAIN")
+    master_node = getConfig("NETWORK", "MASTER_HOSTNAME")
+    master_ip = getConfig("NETWORK", "MASTER_IP")
+    netmask = getConfig("NETWORK", "NETMASK")
+    broadcast = getConfig("NETWORK", "BROADCAST")
+    ip = getConfig("NETWORK", "IP")
+
+    extra_install_cmd = ["pacman",
+                         "--noconfirm",
+                         "-S",
+                         "syslinux",
+                         "iproute2",
+                         "openssh",
+                         "nfs-utils",
+                         "nfsidmap",
+                         "ntp",
+                         "sudo",
+                         "zsh",
+                         "rsync"]
+
+
+    localegen_cmd = ["locale-gen"]
+    logger.debug(localegen)
+
+    localtime_cmd = ["ln", "-s",
+                     "/usr/share/zoneinfo/%s" % zoneInfo,
+                     "/etc/localtime"]
+    logger.debug(localtime_cmd)
+
+    hwclock_cmd = ["hwclock", "--systohc", "--utc"]
+    logger.debug(hwclock_cmd)
+
+    hostname_cmd = ["echo", hostname]
+    logger.debug("%s piped to /etc/hostname" % hostname_cmd)
+    #with open("/etc/hostname", "w") as f:
+        #call(hostname_cmd, stdout=f)
+
+    mkinitcpio_cmd = ["mkinitcpio", "-p", "linux"]
+    logger.debug(mkinitcpio_cmd)
+
+    ## init root passwd?
+
+    shutil.move("/boot/syslinux/syslinux.cfg.REPLACE",
+                "/boot/syslinux/syslinux.cfg")
+
+    syslinux_conf_cmd = ["syslinux-install_update", "-iam"]
+    logger.debug(syslinux_conf_cmd)
+
+    os.path.mkdir("/etc/conf.d")
+    with open("/etc/conf.d/network", "w") as f:
+        f.write("""
+        interface=eth0
+        address=%s
+        netmask=%s
+        broadcast=%s
+        gateway=%s""" % (ip, netmask, broadcast, master_ip))
+    with open("/etc/resolve.conf", "w") as f:
+        f.write("""
+        domain %s
+        nameserver %s""" % (domain, master_ip))
+
+
+    ## setup NFS Home...
+
+    chown_cmd = ["chown", "root:root", "/etc/sudoers.REPLACE"]
+    logger.debug(chwon_cmd)
+    shutil.move("/etc/sudoers.REPLACE", "/etc/sudoers.REPLACE")
+
+    def systemctlEnable(target):
+        systemctl_cmd = ["systemctl", "enable", target]
+        logger.debug(systemctl_cmd)
+
+    systemctlEnable("network.service")
+    systemctlEnable("sshd.service")
+    systemctlEnable("nfsd.service")
+    systemctlEnable("rpc-idmapd.service")
+    systemctlEnable("ntpd.service")
