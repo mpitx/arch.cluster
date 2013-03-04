12b7fde4b62b3523ca2e8d7768f6f0875e4e2099
kennyballou <kballou@onyx.boisestate.edu>
2013-03-04 11:46:46 -0700

    Replace ``logger.debug`` with a local ``call``
    Instead of having ``noop`` be defined by commenting out invokes of
    ``subprocess.call``, a local function should be defined that can forward the
    request if ``noop`` if False. Notice, for the time being, ``noop`` is
    defaulting to True (or don't do anything). This script is still in its infancy;
    I'm not ready for it (nor is the test environment) to start _actually_
    calling/creating sub shells

--- node-install.py
+++ node-install.py
@@ -2,9 +2,8 @@
 
 import os
 import configparser
-from subprocess import call
+import subprocess
 import sys
-import shutil
 import logging
 
 logger = logging.GetLogger("install")
@@ -20,6 +19,13 @@ def getKeys(section):
     config.read("install.config")
     return config[section]
 
+def call(cmd, stdout=None, stderr=None, noop=True, verbose=False):
+    if noop or verbose:
+        logger.debug(cmd)
+    if not noop:
+        return subprocess.call(cmd, stdout=stdout, stderr=stderr)
+    return 0
+
 def prepare_disks():
     ROOT = getConfig("DISK_INFO", "ROOT")
     DEVICES = getKeys("PARTITION_TABLE")
@@ -32,13 +38,11 @@ def partitionDisk(device):
     partitions = partitionTable.split('|')
     logger.debug("Erasing current partition table")
     dd_cmd = ["dd", "if=/dev/zero", "of=%s" %device, "bs=512", "cound=1"]
-    logger.debud(dd_cmd)
-    #call(dd_cmd)
+    call(dd_cmd)
     logger.debug("Initializing new disk label")
     mklabel_cmd = ["parted", device, "mklabel", "gpt"]
-    logger.debug(parted_cmd)
-    #call(mklabel_cmd)
-    for i,part in enumerate(partitions):
+    call(mklabel_cmd)
+    for i, part in enumerate(partitions):
         logger.debug("Creating %d partition" % i)
         options = part.split(";")
         assert 3 <= len(options) <= 3 ##this may later grow
@@ -49,13 +53,12 @@ def partitionDisk(device):
                       options[2],
                       options[1],
                       options[2]]
-        logger.debug(parted_cmd)
-        #call(parted_cmd)
+        call(parted_cmd)
         if option[2] not in ["ext2","ext3","ext4"]:
             continue
         logger.debug("Making FileSystem")
         mkfs_cmd = ["mkfs.%s" % option[2], device]
-        #call(mkfs_cmd)
+        call(mkfs_cmd)
 
 def mount_partitions():
     mounts = getKeys("FSTAB")
@@ -66,10 +69,8 @@ def mount_partitions():
         m = getConfig("FSTAB", mount)
         mkdir_cmd = ["mkdir", "-p", rel_root + m]
         mount_cmd = ["mount", dev, rel_root + m]
-        logger.debug(mkdir_cmd)
-        #call(mkdir_cmd)
-        logger.debug(mount_cmd)
-        #call(mount_cmd)
+        call(mkdir_cmd)
+        call(mount_cmd)
 
 def umount_partitions():
     mounts = getKeys("FSTAB")
@@ -79,36 +80,33 @@ def umount_partitions():
         logger.debug("Unmount %s" % mount)
         m = getConfig("FSTAB", mount)
         umount_cmd = ["umount", m]
-        logger.debug(umount_cmd)
-        #call(umount_cmd)
+        call(umount_cmd)
 
 def bootstrap_system():
     pacstrap_cmd = ["pacstrap", "/mnt", "base", "base-devel"]
-    logger.debug(pacstrap_cmd)
-    #call(pacstrap_cmd)
-    logger.debug("Copying basic config files over to new root")
-    #shutil.copytree("/mntfiles", "/mnt/")
+    call(pacstrap_cmd)
+    rsync_cmd = ["rsync", "-avz", "mntfiles", "/mnt/"]
+    call(rsync_cmd)
     genfstab_cmd = ["genfstab", "-U", "-p", "/mnt"]
-    logger.debug("%s >> /mnt/etc/fstab" % genfstab_cmd)
-    #with open("/mnt/etc/fstab", "a") as fstab:
-        #call(genfstab, stdout=fstab)
+    with open("/mnt/etc/fstab", "a") as fstab:
+        call(genfstab, stdout=fstab)
 
 def live_install_main():
     prepare_disks()
     mount_partitions()
     bootstrap_system()
     logger.debug("Copy and call self from chroot")
-    shutil.copy2(os.join.path(os.getcwd(), "node-install.py"),
-                 "/mnt/root/node-install.py")
+    copy_self_cmd = ["cp", os.path.join(os.getcwd(), "node-install.py"),
+                     "/mnt/root/node-enstall.py"]
+    call(copy_self_cmd)
     chroot_cmd = ["arch-chroot",
                   "/mnt",
                   "python",
                   "node-install.py",
                   "node-number",
                   "in-chroot"]
-    logger.debug(chroot_cmd)
-
-    os.remove("/mnt/root/node-install.py")
+    call(chroot_cmd)
+    call(["rm", "/mnt/root/node-install.py"])
 
     umount_partitions()
 
@@ -134,34 +132,33 @@ def chroot_stage_main():
                          "sudo",
                          "zsh",
                          "rsync"]
-
-
+    call(extra_install_cmd)
     localegen_cmd = ["locale-gen"]
-    logger.debug(localegen)
+    call(localegen_cmd)
 
     localtime_cmd = ["ln", "-s",
                      "/usr/share/zoneinfo/%s" % zoneInfo,
                      "/etc/localtime"]
-    logger.debug(localtime_cmd)
+    call(localtime_cmd)
 
     hwclock_cmd = ["hwclock", "--systohc", "--utc"]
-    logger.debug(hwclock_cmd)
+    call(hwclock_cmd)
 
     hostname_cmd = ["echo", hostname]
     logger.debug("%s piped to /etc/hostname" % hostname_cmd)
-    #with open("/etc/hostname", "w") as f:
-        #call(hostname_cmd, stdout=f)
+    with open("/etc/hostname", "w") as f:
+        call(hostname_cmd, stdout=f)
 
     mkinitcpio_cmd = ["mkinitcpio", "-p", "linux"]
-    logger.debug(mkinitcpio_cmd)
+    call(mkinitcpio_cmd)
 
     ## init root passwd?
 
-    shutil.move("/boot/syslinux/syslinux.cfg.REPLACE",
-                "/boot/syslinux/syslinux.cfg")
+    call(["mv", "/boot/syslinux/syslinux.cfg.REPLACE",
+          "/boot/syslinux/syslinux.cfg"])
 
     syslinux_conf_cmd = ["syslinux-install_update", "-iam"]
-    logger.debug(syslinux_conf_cmd)
+    call(syslinux_conf_cmd)
 
     os.path.mkdir("/etc/conf.d")
     with open("/etc/conf.d/network", "w") as f:
@@ -180,12 +177,12 @@ def chroot_stage_main():
     ## setup NFS Home...
 
     chown_cmd = ["chown", "root:root", "/etc/sudoers.REPLACE"]
-    logger.debug(chwon_cmd)
-    shutil.move("/etc/sudoers.REPLACE", "/etc/sudoers.REPLACE")
+    call(chown_cmd)
+    call(["mv", "/etc/sudoers.REPLACE", "/etc/sudoers.REPLACE"])
 
     def systemctlEnable(target):
         systemctl_cmd = ["systemctl", "enable", target]
-        logger.debug(systemctl_cmd)
+        call(systemctl_cmd)
 
     systemctlEnable("network.service")
     systemctlEnable("sshd.service")
